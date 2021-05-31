a283ca66175804b92976c39b7cc30feded29dd5c
caiorodrigues9 <caiorodrigues9@gmail.com>
2021-05-31 10:18:51 -0300

    Começando com o Laravel

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+
+        $series = [
+            'Lucifer',
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+
+        return view('series.index',compact('series'));
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+}
